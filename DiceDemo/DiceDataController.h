02f2651f3a69cf2c6c0f1ccba1884ac0ac2a3491
Ricardo Rubio <rickarubio@gmail.com>
2014-04-03 18:09:39 -0700

    creates model class for dice and imports it into the view controller

--- DiceDemo/DiceDataController.h
+++ DiceDemo/DiceDataController.h
@@ -1,14 +1,15 @@
 //
-//  ViewController.h
+//  DiceDataController.h
 //  DiceDemo
 //
 //  Created by Ricardo Rubio on 4/3/14.
 //  Copyright (c) 2014 Ricardo Rubio. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "DiceDataController.h"
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
+@interface DiceDataController : NSObject
+
+- (int)getDiceRoll;
 
 @end
--- DiceDemo/DiceDataController.m
+++ DiceDemo/DiceDataController.m
@@ -1,14 +1,19 @@
 //
-//  ViewController.h
+//  DiceDataController.m
 //  DiceDemo
 //
 //  Created by Ricardo Rubio on 4/3/14.
 //  Copyright (c) 2014 Ricardo Rubio. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import "DiceDataController.h"
 
-@interface ViewController : UIViewController
+@implementation DiceDataController
+
+- (int)getDiceRoll
+{
+    int roll = (arc4random() % 6) + 1;
+    return roll;
+}
 
 @end
