addb3e671ba7e9c48b3d9bc0651f606bdac34cef
Ricardo Rubio <rickarubio@gmail.com>
2014-04-03 18:13:22 -0700

    adds a die view which will display one die

---
+++ DiceDemo/DieView.h
@@ -0,0 +1,13 @@
+//
+//  DieView.h
+//  DiceDemo
+//
+//  Created by Ricardo Rubio on 4/3/14.
+//  Copyright (c) 2014 Ricardo Rubio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DieView : UIView
+
+@end
