ead5e0e4b2b85e0c6e34f0cf6dded627b143031a
Ricardo Rubio <rickarubio@gmail.com>
2014-04-03 17:01:56 -0700

    initial commit

---
+++ DiceDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DiceDemo
+//
+//  Created by Ricardo Rubio on 4/3/14.
+//  Copyright (c) 2014 Ricardo Rubio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DiceDemoTests/DiceDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  DiceDemoTests.m
+//  DiceDemoTests
+//
+//  Created by Ricardo Rubio on 4/3/14.
+//  Copyright (c) 2014 Ricardo Rubio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DiceDemoTests : XCTestCase
+
+@end
+
+@implementation DiceDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
